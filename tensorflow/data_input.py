7afe51b431c44335ac64973ff2c464815f404a40
guoyang <guoyang.eric@gmail.com>
2018-12-09 12:32:46 +0800

    Optimize code.

--- tensorflow/data_input.py
+++ tensorflow/data_input.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import json, os, sys
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf 
--- tensorflow/main.py
+++ tensorflow/main.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os, sys, time
+
 import numpy as np 
 import tensorflow as tf 
 import data_input
